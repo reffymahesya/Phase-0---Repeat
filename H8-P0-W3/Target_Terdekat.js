ebad1c34dd789bc790a5ea470e5f8277783fc0b4
reffymahesya <reffymahesyar@gmail.com>
2019-04-05 14:44:37 +0700

    Update Target Terdekat

--- H8-P0-W3/Target_Terdekat.js
+++ H8-P0-W3/Target_Terdekat.js
@@ -1,54 +1,26 @@
 function targetTerdekat(arr) {
   // you can only write your code here!
-var simpan = 0
-var a = arr
-var posisiO = 0;
+var temp = []
+
   for (var i = 0; i < arr.length; i++){
-    if (arr[i] === 'o'){
-      posisiO = i;
-    }
-  }
-  
-  var foundXLeft = false;
-  var xStepLeft = 0;
-  for(var i = posisiO; i >=0; i--){
-   
-    if(arr[i] === 'x'){
-      foundXLeft = true;
-      break;
-    }
-     xStepLeft++;
+    if (arr[i] === 'x'){
+      var calculate = Math.abs(i - arr.indexOf('o'))
+      temp.push(calculate)
+      // console.log(temp)
+    } 
   }
 
-  var foundXRight = false;
-  var xStepRight = 0;
-  for(var i = posisiO; i < arr.length; i++){
-    if(arr[i] === 'x'){
-      foundXRight = true;
-      break;
-    }
-        xStepRight++;
+  if (calculate === undefined){
+    return 0
   }
 
-  if( foundXLeft && foundXRight){
-    if(xStepLeft < xStepRight){
-      return xStepLeft;
-    }
-    else{
-      return xStepRight;
+var min = temp[0]
+  for (var j = 0; j < temp.length; j++){
+    if (temp[j] < min){
+      min = temp[j]
     }
   }
-
-  if(foundXRight){
-    return xStepRight;
-  }
-  if(foundXLeft){
-    return xStepLeft;
-  }
-  if(foundXRight ===  false && foundXLeft === false){
-    return 0;
-  }
-  // return simpan
+  return min
 }
 
 // TEST CASES
@@ -59,7 +31,63 @@ console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
 console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
 
 
-// if (arr[i] === 'x'){
-//   simpan = Math.abs(i - arr.indexOf('o'))
-//   console.log(simpan)   
+// function targetTerdekat(arr) {
+//   // you can only write your code here!
+
+// var simpan = 0
+// var a = arr
+// var posisiO = 0;
+//   for (var i = 0; i < arr.length; i++){
+//     if (arr[i] === 'o'){
+//       posisiO = i;
+//     }
+//   }
+  
+//   var foundXLeft = false;
+//   var xStepLeft = 0;
+//   for(var i = posisiO; i >=0; i--){
+   
+//     if(arr[i] === 'x'){
+//       foundXLeft = true;
+//       break;
+//     }
+//      xStepLeft++;
+//   }
+
+//   var foundXRight = false;
+//   var xStepRight = 0;
+//   for(var i = posisiO; i < arr.length; i++){
+//     if(arr[i] === 'x'){
+//       foundXRight = true;
+//       break;
+//     }
+//         xStepRight++;
+//   }
+
+//   if( foundXLeft && foundXRight){
+//     if(xStepLeft < xStepRight){
+//       return xStepLeft;
+//     }
+//     else{
+//       return xStepRight;
+//     }
+//   }
+
+//   if(foundXRight){
+//     return xStepRight;
+//   }
+//   if(foundXLeft){
+//     return xStepLeft;
+//   }
+//   if(foundXRight ===  false && foundXLeft === false){
+//     return 0;
+//   }
+//   // return simpan
 // }
+
+// // TEST CASES
+// console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+// console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+// console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+// console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+// console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
