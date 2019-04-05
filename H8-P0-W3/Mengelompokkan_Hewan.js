2761d9a48e3e8f155b911846506aad59d5c0da45
reffymahesya <reffymahesyar@gmail.com>
2019-04-05 16:30:55 +0700

    Update Mengelompokkan Hewan

--- H8-P0-W3/Mengelompokkan_Hewan.js
+++ H8-P0-W3/Mengelompokkan_Hewan.js
@@ -1,32 +1,37 @@
 function groupAnimals(animals) {
   // you can only write your code here!
-  var result = []
-  var insideA = []
-  var insideC = []
-  var insideK = []
-  var insideU = []
-
   for (var i = 0; i < animals.length; i++){
-    if (animals[i][0][0] === 'a'){
-      insideA.push(animals[i])
-    } else if (animals[i][0][0] === 'c'){
-      insideC.push(animals[i])
-    } else if (animals[i][0][0] === 'k'){
-      insideK.push(animals[i])
-    } else if (animals[i][0][0] === 'u'){
-      var foundU = true
-      if (foundU){
-        insideU.push(animals[i])
-        result.push(insideU)
+   for (var j = 0; j <= (animals.length - i - 1); j++){
+      if (animals[j] > animals[j + 1]){
+         var temp = animals[j]
+         animals[j] = animals[j + 1]
+         animals[j + 1] = temp
       }
     }
   }
-  result.push(insideA, insideC, insideK)
+
+  var result = []
+  for (var k = 0; k < animals.length; k++){
+    // console.log(animals[k])
+    var found = false
+    for (var l = 0; l < result.length; l++){
+      if (animals[k][0] === result[l][0][0]){
+        found = true
+        result[l].push(animals[k])
+      } 
+    } if (found == false){
+        result.push([animals[k]])
+      }
+  }
   return result
 }
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-[ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+//[ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
 // // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+
+
+//SORT
+//HURUF AWAL NOT DUPLICATE
